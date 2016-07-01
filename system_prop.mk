a6ab68ac8011b5b798c8193956ada7f125b73d01
rago <rago@google.com>
2016-07-01 16:05:00 -0700

    mt6765-common: Set voice call volume steps to 7
    Bug: 28426597
    Change-Id: Icf78d9acec75c500982b1e4391af4d6f3933da37

--- system_prop.mk
+++ system_prop.mk
@@ -1,4 +1,10 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.config.vc_call_vol_steps=7
+
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=mtk-ril.so \
     rild.libargs=-d/dev/ttyC0
+
+
