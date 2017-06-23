a77f0682618af75217dba3bdcbb732bb928c5f36
Jean-Michel Trivi <jmtrivi@google.com>
2017-06-22 19:20:26 -0700

    RMX1801: Media volume: from 15 to 25 steps
    Test: play media, use all 25 vol steps and observe varying attenuation
    Bug: 32195657
    Change-Id: I7ac87df0b2b15de3a7bb87e739bd88ef7b185236
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,6 +1,7 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.config.vc_call_vol_steps=7
+   ro.config.vc_call_vol_steps=7 \
+   ro.config.media_vol_steps=25
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
