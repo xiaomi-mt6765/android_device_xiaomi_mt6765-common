c3bae27ffd7db9f3708657519f6c59b96bf1cbc9
Christopher N. Hesse <raymanfx@gmail.com>
2018-11-26 19:28:54 +0100

    mt6765-common: Hexedit mtk-ril to fix incoming calls
    Taken from: https://github.com/phhusson/treble_experimentations/issues/57#issuecomment-416998086
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -60,4 +60,14 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files-vndk.txt "$SRC" "$SECTION"
 
+# Hack MTK RIL libs for USSD and incoming calls
+# see https://github.com/phhusson/treble_experimentations/issues/57#issuecomment-416998086
+BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+for blob in $BLOB_ROOT/lib/libmtk-ril.so;do
+    sed -i \
+        -e 's/AT+EAIC=2/AT+EAIC=3/g' \
+        $blob
+done
+
 "$MY_DIR"/setup-makefiles.sh
