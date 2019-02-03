a8e93fdb80f8fdf9d3598b8fc02ab7b54adefb9e
Ethan Chen <intervigil@gmail.com>
2019-02-03 15:12:39 -0800

    mt6765-common: Disable verity and descriptor checking
    * avbtool takes a --flags argument, not --flag
    * avbtool provides an option to disable hashtree checking
    
    Change-Id: Ib57d8ebed69a0998005563a9268f9fc736cbeaf7
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,3 +105,4 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
