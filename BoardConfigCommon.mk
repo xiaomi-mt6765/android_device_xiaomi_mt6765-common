497faa86388b9e769c4dc8b92fac1c26456cfb39
Anton Hansson <hansson@google.com>
2019-02-08 17:56:34 +0000

    mt6765-common: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.
    
    Bug: 120773446
    Test: make
    Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,7 +25,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 TARGET_USES_64_BIT_BINDER := true
 
 # APEX
