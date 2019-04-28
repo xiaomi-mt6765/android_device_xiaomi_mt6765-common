48842c109ea74e7a27a511f30d4bddc7a02d1e1d
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    mt6765-common: releasetools: Ship and update vbmeta image
    Change-Id: I8bb4da99945eceae3beab77489e6b9e2a47ab972
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,4 +1,23 @@
+import common
+import re
+
 def FullOTA_InstallEnd(info):
     info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/system", "/system_root");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('unmount("/system_root");');
+    return
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def AddImage(info, basename, dest):
+  name = basename
+  data = info.input_zip.read("IMAGES/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
+def OTA_InstallEnd(info):
+  info.script.Print("Patching firmware images...")
+  AddImage(info, "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta")
+  return
