fac9f8b6f2138c5068f45f940bf32274e498e91d
JarlPenguin <jarlpenguin@outlook.com>
2019-07-28 15:43:27 +0300

    mt6765-common: import framework manifest
    * From MIUI 10.3.3 Global Stable
    
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>
    Change-Id: I5714bcc4e33fad96104fb6db4120a8cee61e4510

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,6 +4,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 COMMON_PATH := device/xiaomi/mt6765-common
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 
 # Architecture
 TARGET_ARCH := arm
