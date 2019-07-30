7735816adc98ba32b9d2aba4ddc249f03d801bbb
Sᴀᴍᴀʀ Vɪꜱᴘᴜᴛᴇ <samarvispute121@gmail.com>
2019-07-30 14:42:21 +0530

    mt6765-common: patch_ril: Remove non-existant 64bit libraries

--- rootdir/bin/patch_ril.sh
+++ rootdir/bin/patch_ril.sh
@@ -1,6 +1,3 @@
 #!/sbin/sh
-cp /system/lib/libmtk-ril.so /vendor/lib/libmtk-ril.so
-cp /system/lib/librilfusion.so /vendor/lib/librilfusion.so
-mkdir -p /vendor/lib64/
-cp /system/lib64/libmtk-ril.so /vendor/lib64/libmtk-ril.so
-cp /system/lib64/librilfusion.so /vendor/lib64/librilfusion.so
+cp -f /system/lib/libmtk-ril.so /vendor/lib/libmtk-ril.so
+cp -f /system/lib/librilfusion.so /vendor/lib/librilfusion.so
