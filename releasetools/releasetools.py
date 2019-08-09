d892f2a7854b156aabcda7efab503ec0d04b9c50
Sᴀᴍᴀʀ Vɪꜱᴘᴜᴛᴇ <samarvispute121@gmail.com>
2019-08-09 12:51:54 +0530

    mt6765-common: Update releasetools.py

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -2,6 +2,3 @@ def FullOTA_InstallEnd(info):
     info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/system", "/system");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('unmount("/system");');
-    info.script.AppendExtra('mount("ext4", "EMCC", "/dev/block/platform/bootdevice/by-name/vendor", "/vendor");');
-    info.script.AppendExtra('run_program("/sbin/sh", "/system/bin/patch_ril.sh");');
-    info.script.AppendExtra('unmount("/vendor");');
