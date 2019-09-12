0f4eb083038b5d013e6749570a2b2a8ba89e088a
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-12 23:47:56 +0200

    mt6765-common: Enabled updateable APEX
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3

--- mt6765.mk
+++ mt6765.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
