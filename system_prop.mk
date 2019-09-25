4dc51dc16289141ab59118d5b466edb75dda796f
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    mt6765-common: Convert remaining configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -10,3 +10,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.rild.libpath=mtk-ril.so \
     vendor.rild.libargs=-d/dev/ttyC0
+
+# Surfaceflinger
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+   ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+   ro.surface_flinger.max_frame_buffer_acquired_buffers=3
