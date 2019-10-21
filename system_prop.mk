8fa81cb40632cee91ed892a740a520efc93ece9a
SamarV-121 <samarvispute121@gmail.com>
2019-10-21 07:49:06 +0000

    mt6765-common: Move props to /system/etc/prop.default
    Change-Id: I2aebd05f4f38f7e33a50842cab5f45d048f3d10c
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,17 +1,17 @@
 # system.prop for mt6765-common
 
 # Audio
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.config.vc_call_vol_steps=7 \
    ro.config.media_vol_steps=25
 
 # Performance
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
 
 # RIL
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.rild.libpath=mtk-ril.so \
     vendor.rild.libargs=-d/dev/ttyC0
