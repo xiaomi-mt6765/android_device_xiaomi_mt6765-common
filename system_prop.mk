5727214c4f5ac7dfbffa917490bad25c7665514f
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-26 02:57:13 +0300

    mt6765-common: set MediaTek perf props
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ie47b4d3437b0bb11dfec5a417c2954ad387041a6

--- system_prop.mk
+++ system_prop.mk
@@ -5,9 +5,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
    ro.config.vc_call_vol_steps=7 \
    ro.config.media_vol_steps=25
 
+# Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mtk_perf_simple_start_win=1 \
+    ro.mtk_perf_fast_start_win=1 \
+    ro.mtk_perf_response_time=1
+
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.rild.libpath=mtk-ril.so \
     vendor.rild.libargs=-d/dev/ttyC0
-
-
