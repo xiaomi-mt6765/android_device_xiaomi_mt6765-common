c1bd1844e4e78cad28e506a031ed1cc04fd5a8d3
JarlPenguin <jarlpenguin@outlook.com>
2019-11-05 21:46:06 +0300

    mt6765-common: releasetools: use the actual system mount point instead of the symlink
    * Some recoveries don't symlink /system_root/system to /system, so use the /system_root/system mount point instead
    
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -3,7 +3,7 @@ import re
 
 def FullOTA_InstallEnd(info):
     info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/system", "/system_root");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('unmount("/system_root");');
     return
 
