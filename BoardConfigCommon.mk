3650b269e2bcfe30020446849c7bb7795084ad05
Myst33d <45560476+myst33d@users.noreply.github.com>
2020-02-17 17:44:40 +0600

    mt6765-common: Fix typo

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,5 +104,5 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
