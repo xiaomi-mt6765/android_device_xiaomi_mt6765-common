950519be1ec441391840225776178cd79b0365dc
Myst33d #RoccoIsCute <myst33d@gmail.com>
2020-03-27 19:08:55 +0600

    Revert "mt6765-common: Remove deprecated flag"

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.target.rc
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/init.target.rc
 LOCAL_MODULE_RELATIVE_PATH := init
