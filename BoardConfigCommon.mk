d191419cfa2909e39fb482a5b9c6465bdaf2a2cb
Yaroslav Popkov <yarikpopkov1@gmail.com>
2020-04-01 13:35:15 +0500

    arch fix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,10 +20,10 @@ COMMON_PATH := device/xiaomi/mt6765-common
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
 # Audio
