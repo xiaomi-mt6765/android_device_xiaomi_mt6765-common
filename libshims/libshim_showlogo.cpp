dcfdc7ed7018b3edb8eac6f52248805d080ce870
SamarV-121 <samarvispute121@gmail.com>
2020-10-02 12:38:27 +0530

    [WIP]update showlogo shim for R
    https://github.com/LineageOS/android_frameworks_native/blob/fd409e29605411dfbd12b004e416e6ca9b43749e/libs/gui/SurfaceComposerClient.cpp#L1600
    https://github.com/LineageOS/android_frameworks_native/commit/e8097ca7c5e00ed61d7f604d8eda8d2fe2d2fa9b#diff-9236cd0a72ffc463a8501ba6d0c9f3b5
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -1,11 +1,12 @@
 #include <gui/SurfaceComposerClient.h>
 
 namespace android {
-    extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, android::LayerMetadata metadata);
+
+    extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, android::LayerMetadata metadata, uint32_t* outTransformHint);
 
     extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEii(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, int32_t windowType, int32_t ownerUid) {
         android::LayerMetadata metadata;
-        _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(name, w, h, format, flags, nullptr, metadata);
+        _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
     }
 
     extern "C" void _ZN7android21SurfaceComposerClient23getInternalDisplayTokenEv(void);
@@ -18,6 +19,14 @@ namespace android {
 
     extern "C" void _ZN7android14SurfaceControl5clearEv(void){
         _ZN7android14SurfaceControl7destroyEv();
+    }
+
+    extern "C" void SurfaceControl::destroy()
+    {
+        if (isValid()) {
+        SurfaceComposerClient::Transaction().reparent(this, nullptr).apply();
+        }
+        release();
     }
 
     extern "C" void _ZN7android4base10StartsWithERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPKc() { }
